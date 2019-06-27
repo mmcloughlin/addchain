abe08085dd1e537ccdaab978c2f47028ad451705
Michael McLoughlin <mmcloughlin@gmail.com>
2019-06-27 10:47:42 -0700

    addchain: limit time for primes tests
    Extracted-from: mmcloughlin/ec3@864950929405cd0f995446e7d9d0ced16b744039

--- internal/test/util.go
+++ internal/test/util.go
@@ -29,25 +29,26 @@ func trialsduration() time.Duration {
 	}
 }
 
-// Repeat the given trial function. The duration is controlled by custom command-line flags.
+// Repeat the given trial function. The duration is controlled by custom
+// command-line flags. The trial function returns whether it wants to continue
+// testing.
 //
 //	-short		run for less time than usual
 //	-long		allow more time
 //	-stress		run for an extremely long time
 //	-duration	set a custom duration
-func Repeat(t *testing.T, trial func(t *testing.T)) {
+func Repeat(t *testing.T, trial func(t *testing.T) bool) {
 	start := time.Now()
 	d := trialsduration()
-	n := 0
-	for time.Since(start) < d {
-		trial(t)
+	n := 1
+	for time.Since(start) < d && trial(t) {
 		n++
 	}
 	t.Logf("%d trials in %s", n, time.Since(start))
 }
 
 // Trials returns a function that repeats f.
-func Trials(f func(t *testing.T)) func(t *testing.T) {
+func Trials(f func(t *testing.T) bool) func(t *testing.T) {
 	return func(t *testing.T) {
 		Repeat(t, f)
 	}
--- search_test.go
+++ search_test.go
@@ -26,7 +26,7 @@ func TestChainAlgorithms(t *testing.T) {
 func ChainAlgorithmSuite(a ChainAlgorithm) func(t *testing.T) {
 	return func(t *testing.T) {
 		t.Run("powers_of_two", CheckPowersOfTwo(a, 100))
-		t.Run("random_int64", test.Trials(CheckRandomInt64(a)))
+		t.Run("random_int64", CheckRandomInt64s(a))
 		t.Run("primes", CheckPrimes(a))
 	}
 }
@@ -41,18 +41,26 @@ func CheckPowersOfTwo(a ChainAlgorithm, e int) func(t *testing.T) {
 	}
 }
 
-func CheckRandomInt64(a ChainAlgorithm) func(t *testing.T) {
-	return func(t *testing.T) {
+func CheckRandomInt64s(a ChainAlgorithm) func(t *testing.T) {
+	return test.Trials(func(t *testing.T) bool {
 		r := rand.Int63n(math.MaxInt64)
 		n := big.NewInt(r)
 		AssertChainAlgorithmProduces(t, a, n)
-	}
+		return true
+	})
 }
 
 func CheckPrimes(a ChainAlgorithm) func(t *testing.T) {
-	return func(t *testing.T) {
-		for _, p := range prime.Distinguished {
-			AssertChainAlgorithmProduces(t, a, p.Int())
-		}
+	// Prepare primes in a random order.
+	ps := []*big.Int{}
+	for _, p := range prime.Distinguished {
+		ps = append(ps, p.Int())
 	}
+	rand.Shuffle(len(ps), func(i, j int) { ps[i], ps[j] = ps[j], ps[i] })
+
+	return test.Trials(func(t *testing.T) bool {
+		AssertChainAlgorithmProduces(t, a, ps[0])
+		ps = ps[1:]
+		return len(ps) > 0
+	})
 }
