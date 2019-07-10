3a1472e30d8eabf7533e85dbfb4d63beb3351574
Michael McLoughlin <mmcloughlin@gmail.com>
2019-07-09 22:52:21 -0700

    efd/op3/parse: test expected results
    Closes mmcloughlin/ec3#37
    
    Extracted-from: mmcloughlin/ec3@f59adb473389822902b85ef310de42feb3d78b5d

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -4,7 +4,16 @@ import "testing"
 
 // NoError fails the test on a non-nil error.
 func NoError(t *testing.T, err error) {
+	t.Helper()
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+// Error fails the test on a nil error.
+func Error(t *testing.T, err error) {
+	t.Helper()
+	if err == nil {
+		t.Fatal("expected error; got nil")
 	}
 }
