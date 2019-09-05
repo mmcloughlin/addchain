b145e16461f20eb75d2cbcbbd56e39f06be6e223
Michael McLoughlin <mmcloughlin@gmail.com>
2019-09-04 18:06:32 -0700

    internal/errutil: multi-error type
    Extracted-from: mmcloughlin/ec3@10d63e275b7c30f39845ffdf5ab9b87ba7746353

--- internal/errutil/errors.go
+++ internal/errutil/errors.go
@@ -1,6 +1,10 @@
 package errutil
 
-import "golang.org/x/xerrors"
+import (
+	"fmt"
+
+	"golang.org/x/xerrors"
+)
 
 // ErrNotImplemented is returned when feature is currently not implemented.
 var ErrNotImplemented = xerrors.New("not implemented")
@@ -14,4 +18,32 @@ func AssertionFailure(format string, args ...interface{}) error {
 // UnexpectedType builds an error for an unexpected type, typically in a type switch.
 func UnexpectedType(t interface{}) error {
 	return AssertionFailure("unexpected type %T", t)
+}
+
+// Errors is a collection of errors.
+type Errors []error
+
+// Add appends an error to the list.
+func (e *Errors) Add(err error) {
+	*e = append(*e, err)
+}
+
+// Err returns an error equivalent to this error list.
+// If the list is empty, Err returns nil.
+func (e Errors) Err() error {
+	if len(e) == 0 {
+		return nil
+	}
+	return e
+}
+
+// Error implements the error interface.
+func (e Errors) Error() string {
+	switch len(e) {
+	case 0:
+		return "no errors"
+	case 1:
+		return e[0].Error()
+	}
+	return fmt.Sprintf("%s (and %d more errors)", e[0], len(e)-1)
 }
