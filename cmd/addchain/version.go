cb5d49ed1a2b7dc5ec18e653818f127e9dd47483
Michael McLoughlin <mmcloughlin@gmail.com>
2021-03-31 00:04:43 -0700

    cmd/addchain,internal/meta: build version
    Updates #64

---
+++ cmd/addchain/version.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"runtime"
+
+	"github.com/google/subcommands"
+
+	"github.com/mmcloughlin/addchain/internal/cli"
+)
+
+// version subcommand.
+type version struct {
+	version string
+
+	cli.Command
+}
+
+func (*version) Name() string     { return "version" }
+func (*version) Synopsis() string { return "print addchain version" }
+func (*version) Usage() string {
+	return `Usage: version [<filename>]
+
+Print the version of the addchain tool.
+
+ `
+}
+
+func (cmd *version) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) (status subcommands.ExitStatus) {
+	fmt.Printf("addchain version %s %s/%s\n", cmd.version, runtime.GOOS, runtime.GOARCH)
+	return subcommands.ExitSuccess
+}
