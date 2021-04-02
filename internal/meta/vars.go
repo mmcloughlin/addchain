0b83841503b094159cfdd0efbd879bd01bf4b7a6
Michael McLoughlin <mmcloughlin@gmail.com>
2021-04-01 22:23:01 -0700

    add zenodo id var

--- internal/meta/vars.go
+++ internal/meta/vars.go
@@ -6,5 +6,5 @@ var (
 	releasedate    = "2021-03-21"
 	conceptdoi     = "10.5281/zenodo.4625263"
 	doi            = "10.5281/zenodo.4625264"
-	zenodoid       = "756937"
+	zenodoid       = "4625264"
 )
