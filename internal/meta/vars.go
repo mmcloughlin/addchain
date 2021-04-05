f448b917c9ce5ab2d90074dbd7b437dce00df0d2
Michael McLoughlin <mmcloughlin@gmail.com>
2021-04-05 00:27:04 -0700

    all: release 0.2.0

--- internal/meta/vars.go
+++ internal/meta/vars.go
@@ -2,9 +2,9 @@ package meta
 
 var (
 	buildversion   = ""
-	releaseversion = "0.1.0"
-	releasedate    = "2021-03-21"
+	releaseversion = "0.2.0"
+	releasedate    = "2021-04-05"
 	conceptdoi     = "10.5281/zenodo.4625263"
-	doi            = "10.5281/zenodo.4625264"
-	zenodoid       = "4625264"
+	doi            = "10.5281/zenodo.4662389"
+	zenodoid       = "4662389"
 )
