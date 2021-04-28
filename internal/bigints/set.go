75a34ce4c28b04de802c226e09292822db84bc87
Michael McLoughlin <mmcloughlin@gmail.com>
2021-04-27 23:31:53 -0700

    internal/bigints: reduce allocations in set key

--- internal/bigints/set.go
+++ internal/bigints/set.go
@@ -1,6 +1,10 @@
 package bigints
 
-import "math/big"
+import (
+	"math/big"
+	"strconv"
+	"sync"
+)
 
 type Set struct {
 	set map[string]bool
@@ -19,13 +23,46 @@ func NewSet(xs ...*big.Int) *Set {
 }
 
 func (s *Set) Add(x *big.Int) {
-	s.set[s.key(x)] = true
+	s.set[setkey(x)] = true
 }
 
 func (s *Set) Contains(x *big.Int) bool {
-	return s.set[s.key(x)]
+	b := bufpool.Get().(*buffer)
+	defer bufpool.Put(b)
+	b.buf = appendkey(b.buf[:0], x)
+	return s.set[string(b.buf)]
 }
 
-func (s *Set) key(x *big.Int) string {
-	return x.Text(32)
+func setkey(x *big.Int) string {
+	var buf []byte
+	buf = appendkey(buf, x)
+	return string(buf)
+}
+
+func appendkey(buf []byte, x *big.Int) []byte {
+	w := x.Bits()
+
+	// Skip leading zeros.
+	i := len(w) - 1
+	for ; i >= 0 && w[i] == 0; i-- {
+	}
+
+	// Format remaining.
+	for ; i >= 0; i-- {
+		buf = strconv.AppendUint(buf, uint64(w[i]), 16)
+	}
+
+	return buf
+}
+
+type buffer struct {
+	buf []byte
+}
+
+var bufpool = sync.Pool{
+	New: func() interface{} {
+		return &buffer{
+			buf: make([]byte, 0, 32),
+		}
+	},
 }
