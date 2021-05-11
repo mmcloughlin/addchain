1497ee46ce3ab9b1f0e82d3318b29600cc214e93
Michael McLoughlin <mmcloughlin@gmail.com>
2021-05-11 16:03:43 -0700

    internal/bigvector: use interface type

--- internal/bigvector/bigvector.go
+++ internal/bigvector/bigvector.go
@@ -3,48 +3,77 @@ package bigvector
 
 import (
 	"math/big"
+
+	"github.com/mmcloughlin/addchain/internal/bigint"
 )
 
-// Vector is a vector of big integers.
-type Vector []big.Int
+var (
+	zero = bigint.Zero()
+	one  = bigint.One()
+)
+
+type Vector interface {
+	Len() int
+	Idx(i int) *big.Int
+}
 
 // New constructs an n-dimensional zero vector.
 func New(n int) Vector {
-	v := make(Vector, n)
-	return v
+	return make(vector, n)
 }
+
+type vector []big.Int
+
+func (v vector) Len() int           { return len(v) }
+func (v vector) Idx(i int) *big.Int { return &v[i] }
 
 // NewBasis constructs an n-dimensional basis vector with a 1 in position i.
 func NewBasis(n, i int) Vector {
-	v := New(n)
-	v[i].SetInt64(1)
-	return v
+	return basis{n: n, i: i}
+}
+
+type basis struct {
+	n int
+	i int
+}
+
+func (b basis) Len() int { return b.n }
+
+func (b basis) Idx(i int) *big.Int {
+	switch {
+	case i >= b.n:
+		panic("out of range")
+	case i == b.i:
+		return one
+	default:
+		return zero
+	}
 }
 
 // Add vectors.
 func Add(u, v Vector) Vector {
 	assertsamelen(u, v)
-	n := len(u)
-	w := make(Vector, n)
+	n := u.Len()
+	w := make(vector, n)
 	for i := 0; i < n; i++ {
-		w[i].Add(&u[i], &v[i])
+		w[i].Add(u.Idx(i), v.Idx(i))
 	}
 	return w
 }
 
 // Lsh left shifts every element of the vector v.
 func Lsh(v Vector, s uint) Vector {
-	n := len(v)
-	w := make(Vector, n)
+	n := v.Len()
+	w := make(vector, n)
 	for i := 0; i < n; i++ {
-		w[i].Lsh(&v[i], s)
+		w[i].Lsh(v.Idx(i), s)
 	}
 	return w
 }
 
 // assertsamelen panics if u and v are different lengths.
 func assertsamelen(u, v Vector) {
-	if len(u) != len(v) {
+	if u.Len() != v.Len() {
 		panic("bigvector: length mismatch")
 	}
 }
