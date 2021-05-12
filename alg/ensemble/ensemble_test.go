10bb4ec2704d49db428e296621252a68d7546e92
Michael McLoughlin <mmcloughlin@gmail.com>
2021-05-11 21:36:08 -0700

    alg/ensemble: benchmark (#97)
    Updates #60
    Updates #25

---
+++ alg/ensemble/ensemble_test.go
@@ -0,0 +1,24 @@
+package ensemble
+
+import (
+	"testing"
+
+	"github.com/mmcloughlin/addchain/internal/results"
+)
+
+func BenchmarkResults(b *testing.B) {
+	as := Ensemble()
+	for _, c := range results.Results {
+		c := c // scopelint
+		n := c.Target()
+		b.Run(c.Slug, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				for _, a := range as {
+					if _, err := a.FindChain(n); err != nil {
+						b.Fatal(err)
+					}
+				}
+			}
+		})
+	}
+}
