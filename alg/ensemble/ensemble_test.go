2676db6391c4158cf655a961102ead9e3349f683
Michael McLoughlin <mmcloughlin@gmail.com>
2021-05-12 18:17:04 -0700

    alg/ensemble: test results unchanged

--- alg/ensemble/ensemble_test.go
+++ alg/ensemble/ensemble_test.go
@@ -1,10 +1,68 @@
 package ensemble
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"reflect"
 	"testing"
 
+	"github.com/mmcloughlin/addchain/alg/exec"
 	"github.com/mmcloughlin/addchain/internal/results"
+	"github.com/mmcloughlin/addchain/internal/test"
 )
+
+func TestResults(t *testing.T) {
+	as := Ensemble()
+	for _, c := range results.Results {
+		c := c // scopelint
+		t.Run(c.Slug, func(t *testing.T) {
+			t.Parallel()
+
+			// Execute.
+			ex := exec.NewParallel()
+			rs := ex.Execute(c.Target(), as)
+
+			// Summarize results in map from algorithm name to program length.
+			got := map[string]int{}
+			for _, r := range rs {
+				if r.Err != nil {
+					t.Fatalf("error with %s: %v", r.Algorithm, r.Err)
+				}
+				got[r.Algorithm.String()] = len(r.Program)
+			}
+
+			// If golden, write out results data file.
+			filename := test.GoldenName(c.Slug)
+
+			if test.Golden() {
+				t.Logf("writing golden file %s", filename)
+				b, err := json.MarshalIndent(got, "", "\t")
+				if err != nil {
+					t.Fatal(err)
+				}
+				if err := ioutil.WriteFile(filename, b, 0644); err != nil {
+					t.Fatalf("write golden file: %v", err)
+				}
+			}
+
+			// Load golden file.
+			b, err := ioutil.ReadFile(filename)
+			if err != nil {
+				t.Fatalf("read golden file: %v", err)
+			}
+
+			var expect map[string]int
+			if err := json.Unmarshal(b, &expect); err != nil {
+				t.Fatal(err)
+			}
+
+			// Verify equal.
+			if !reflect.DeepEqual(expect, got) {
+				t.Fatal("results do not match golden file")
+			}
+		})
+	}
+}
 
 func BenchmarkResults(b *testing.B) {
 	as := Ensemble()
