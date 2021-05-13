05190c7a332a9f392f033496739cc3fbef6c19ec
Michael McLoughlin <mmcloughlin@gmail.com>
2021-05-12 23:29:59 -0700

    internal/bigvector: use interface to allocations for basis vectors (#106)
    By far the largest number of allocations comes from the creation of bigvector
    basis vectors. These just contain zeros and ones. This PR changes the Vector
    type to an interface so that the basis type can be implemented without any
    allocations at all.
    
    Unfortunately, this does complicate the interface, since it requires the user
    to not write to any integers returned from Idx(). Consider this okay since
    it's an internal package.
    
    Updates #60
    Updates #25

--- internal/bigvector/bigvector.go
+++ internal/bigvector/bigvector.go
@@ -1,4 +1,5 @@
-// Package bigvector implements operations on vectors of multi-precision integers.
+// Package bigvector implements operations on vectors of immutable
+// multi-precision integers.
 package bigvector
 
 import (
@@ -7,49 +8,79 @@ import (
 	"github.com/mmcloughlin/addchain/internal/bigint"
 )
 
-// Vector is a vector of big integers.
-type Vector []*big.Int
+// Vector of immutable multi-precision integers.
+type Vector interface {
+	// Len returns vector length.
+	Len() int
+
+	// Idx returns integer at index i. Returned integer must not be written to.
+	Idx(i int) *big.Int
+}
 
 // New constructs an n-dimensional zero vector.
 func New(n int) Vector {
-	v := make(Vector, n)
-	for i := 0; i < n; i++ {
-		v[i] = bigint.Zero()
-	}
-	return v
+	return make(vector, n)
 }
+
+type vector []big.Int
+
+func (v vector) Len() int           { return len(v) }
+func (v vector) Idx(i int) *big.Int { return &v[i] }
 
 // NewBasis constructs an n-dimensional basis vector with a 1 in position i.
 func NewBasis(n, i int) Vector {
-	v := New(n)
-	v[i] = bigint.One()
-	return v
+	return basis{n: n, i: i}
+}
+
+// Basis implementation saves allocations by returning pre-allocated zero and
+// one integers based on the index requested.
+var (
+	zero = bigint.Zero()
+	one  = bigint.One()
+)
+
+type basis struct {
+	n int
+	i int
+}
+
+func (b basis) Len() int { return b.n }
+
+func (b basis) Idx(i int) *big.Int {
+	switch {
+	case i >= b.n:
+		panic("bigvector: index out of range")
+	case i == b.i:
+		return one
+	default:
+		return zero
+	}
 }
 
 // Add vectors.
 func Add(u, v Vector) Vector {
 	assertsamelen(u, v)
-	n := len(u)
-	w := make(Vector, n)
+	n := u.Len()
+	w := make(vector, n)
 	for i := 0; i < n; i++ {
-		w[i] = new(big.Int).Add(u[i], v[i])
+		w[i].Add(u.Idx(i), v.Idx(i))
 	}
 	return w
 }
 
 // Lsh left shifts every element of the vector v.
 func Lsh(v Vector, s uint) Vector {
-	n := len(v)
-	w := make(Vector, n)
+	n := v.Len()
+	w := make(vector, n)
 	for i := 0; i < n; i++ {
-		w[i] = new(big.Int).Lsh(v[i], s)
+		w[i].Lsh(v.Idx(i), s)
 	}
 	return w
 }
 
 // assertsamelen panics if u and v are different lengths.
 func assertsamelen(u, v Vector) {
-	if len(u) != len(v) {
+	if u.Len() != v.Len() {
 		panic("bigvector: length mismatch")
 	}
 }
