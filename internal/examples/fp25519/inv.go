0bebcbea2b25e0a6616ff29f588bcb44ee43ad99
Michael McLoughlin <mmcloughlin@gmail.com>
2021-10-26 21:35:07 -0700

    spruce up example codegen

--- internal/examples/fp25519/inv.go
+++ internal/examples/fp25519/inv.go
@@ -1,6 +1,27 @@
 package fp25519
 
+// Inv computes z = 1/x (mod p) and returns it.
 func (z *Elt) Inv(x *Elt) *Elt {
+	// Inversion computation is derived from the addition chain:
+	//
+	//	_10       = 2*1
+	//	_11       = 1 + _10
+	//	_1100     = _11 << 2
+	//	_1111     = _11 + _1100
+	//	_11110000 = _1111 << 4
+	//	_11111111 = _1111 + _11110000
+	//	x10       = _11111111 << 2 + _11
+	//	x20       = x10 << 10 + x10
+	//	x30       = x20 << 10 + x10
+	//	x60       = x30 << 30 + x30
+	//	x120      = x60 << 60 + x60
+	//	x240      = x120 << 120 + x120
+	//	x250      = x240 << 10 + x10
+	//	return      (x250 << 2 + 1) << 3 + _11
+	//
+	// Operations: 254 squares 12 multiplies
+
+	// Allocate Temporaries.
 	var (
 		t0 = new(Elt)
 		t1 = new(Elt)
