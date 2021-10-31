c1b2b80ceb8cf4ce74bdd78c8f5bfbc3572bfbea
Michael McLoughlin <mmcloughlin@gmail.com>
2021-10-30 22:00:13 -0700

    first version of Interpreter

--- acc/eval/interp.go
+++ acc/eval/interp.go
@@ -6,6 +6,7 @@ import (
 	"math/big"
 
 	"github.com/mmcloughlin/addchain/acc/ir"
+	"github.com/mmcloughlin/addchain/internal/errutil"
 )
 
 type Interpreter struct {
@@ -18,13 +19,13 @@ func NewInterpreter() *Interpreter {
 	}
 }
 
-// Load the variable v.
+// Load the named variable.
 func (i *Interpreter) Load(v string) (*big.Int, bool) {
 	x, ok := i.state[v]
 	return x, ok
 }
 
-// Store x into the variable v.
+// Store x into the named variable.
 func (i *Interpreter) Store(v string, x *big.Int) {
 	i.state[v] = x
 }
@@ -49,102 +50,60 @@ func (i *Interpreter) Execute(p *ir.Program) error {
 }
 
 func (i *Interpreter) instruction(inst *ir.Instruction) error {
+	output := i.output(inst.Output)
+	switch op := inst.Op.(type) {
+	case ir.Add:
+		x, err := i.operands(op.X, op.Y)
+		if err != nil {
+			return err
+		}
+		output.Add(x[0], x[1])
+	case ir.Double:
+		x, err := i.operand(op.X)
+		if err != nil {
+			return err
+		}
+		output.Add(x, x)
+	case ir.Shift:
+		x, err := i.operand(op.X)
+		if err != nil {
+			return err
+		}
+		output.Lsh(x, op.S)
+	default:
+		return errutil.UnexpectedType(op)
+	}
 	return nil
 }
 
-//
-// func (e *Evaluator) assignment(a ast.Assignment) error {
-// 	lhs := e.dst(a.LHS)
-// 	switch expr := a.RHS.(type) {
-// 	case ast.Pow:
-// 		x, err := e.operands(expr.X, expr.N)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		lhs.Exp(x[0], x[1], e.m)
-// 	case ast.Inv:
-// 		x, err := e.operand(expr.X)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		lhs.ModInverse(x, e.m)
-// 	case ast.Mul:
-// 		x, err := e.operands(expr.X, expr.Y)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		lhs.Mul(x[0], x[1])
-// 	case ast.Neg:
-// 		x, err := e.operand(expr.X)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		lhs.Neg(x)
-// 	case ast.Add:
-// 		x, err := e.operands(expr.X, expr.Y)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		lhs.Add(x[0], x[1])
-// 	case ast.Sub:
-// 		x, err := e.operands(expr.X, expr.Y)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		lhs.Sub(x[0], x[1])
-// 	case ast.Cond:
-// 		x, err := e.operands(expr.X, expr.C)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		if x[1].Sign() != 0 {
-// 			lhs.Set(x[0])
-// 		}
-// 	case ast.Variable, ast.Constant:
-// 		x, err := e.operand(expr)
-// 		if err != nil {
-// 			return err
-// 		}
-// 		lhs.Set(x)
-// 	default:
-// 		return errutil.UnexpectedType(expr)
-// 	}
-// 	lhs.Mod(lhs, e.m)
-// 	return nil
-// }
-//
-// func (e Evaluator) dst(v ast.Variable) *big.Int {
-// 	if x, ok := e.Load(v); ok {
-// 		return x
-// 	}
-// 	x := new(big.Int)
-// 	e.Store(v, x)
-// 	return x
-// }
-//
-// func (e *Evaluator) operands(operands ...ast.Operand) ([]*big.Int, error) {
-// 	xs := make([]*big.Int, 0, len(operands))
-// 	for _, operand := range operands {
-// 		x, err := e.operand(operand)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		xs = append(xs, x)
-// 	}
-// 	return xs, nil
-// }
-//
-// func (e *Evaluator) operand(operand ast.Operand) (*big.Int, error) {
-// 	switch op := operand.(type) {
-// 	case ast.Variable:
-// 		x, ok := e.Load(op)
-// 		if !ok {
-// 			return nil, xerrors.Errorf("variable %q is not defined", op)
-// 		}
-// 		return x, nil
-// 	case ast.Constant:
-// 		return new(big.Int).SetUint64(uint64(op)), nil
-// 	default:
-// 		return nil, errutil.UnexpectedType(op)
-// 	}
-// }
+func (i *Interpreter) output(operand *ir.Operand) *big.Int {
+	if x, ok := i.Load(operand.Identifier); ok {
+		return x
+	}
+	x := new(big.Int)
+	i.Store(operand.Identifier, x)
+	return x
+}
+
+func (i *Interpreter) operands(operands ...*ir.Operand) ([]*big.Int, error) {
+	xs := make([]*big.Int, 0, len(operands))
+	for _, operand := range operands {
+		x, err := i.operand(operand)
+		if err != nil {
+			return nil, err
+		}
+		xs = append(xs, x)
+	}
+	return xs, nil
+}
+
+func (i *Interpreter) operand(operand *ir.Operand) (*big.Int, error) {
+	if operand.Identifier == "" {
+		return nil, fmt.Errorf("operand %s missing identifier", operand)
+	}
+	x, ok := i.Load(operand.Identifier)
+	if !ok {
+		return nil, fmt.Errorf("operand %q is not defined", operand)
+	}
+	return x, nil
+}
