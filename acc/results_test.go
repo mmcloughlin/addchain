ef3e014f5d46a063501ce6c9b6904ae870f2225c
Michael McLoughlin <mmcloughlin@gmail.com>
2021-11-01 23:45:05 -0700

    acc: results test (#136)
    Adds an integration test designed to confirm we can produce correct code for
    all the target exponents in the results list. Specifically, for every target
    exponent and every algorithm in the ensemble, we confirm the resulting chain
    can be converted to an addition chain program, allocated variables, and
    interpreted to give the expected result.
    
    This test makes use of the new test execution timer added in #135, so it will
    only execute tests for the exponents it has time for.
    
    Updates #129

---
+++ acc/results_test.go
@@ -0,0 +1,78 @@
+package acc
+
+import (
+	"testing"
+
+	"github.com/mmcloughlin/addchain/acc/eval"
+	"github.com/mmcloughlin/addchain/acc/pass"
+	"github.com/mmcloughlin/addchain/alg/ensemble"
+	"github.com/mmcloughlin/addchain/alg/exec"
+	"github.com/mmcloughlin/addchain/internal/bigint"
+	"github.com/mmcloughlin/addchain/internal/results"
+	"github.com/mmcloughlin/addchain/internal/test"
+)
+
+// TestResults is an integration test designed to confirm we can produce correct
+// code for all the target exponents in the results list. Specifically, for
+// every target exponent and every algorithm in the ensemble, we confirm the
+// resulting chain can be converted to an addition chain program, allocated
+// variables, and interpreted to give the expected result.
+func TestResults(t *testing.T) {
+	timer := test.Start()
+	as := ensemble.Ensemble()
+	for _, c := range results.Results {
+		c := c // scopelint
+		t.Run(c.Slug, func(t *testing.T) {
+			timer.Check(t)
+
+			// Execute.
+			ex := exec.NewParallel()
+			rs := ex.Execute(c.Target(), as)
+
+			// Check each result.
+			for _, r := range rs {
+				if r.Err != nil {
+					t.Fatalf("error with %s: %v", r.Algorithm, r.Err)
+				}
+
+				// Decompile into IR.
+				p, err := Decompile(r.Program)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				// Allocate.
+				a := pass.Allocator{
+					Input:  "x",
+					Output: "z",
+					Format: "t%d",
+				}
+				if err := a.Execute(p); err != nil {
+					t.Fatal(err)
+				}
+
+				// Evaluate.
+				i := eval.NewInterpreter()
+				x := bigint.One()
+				i.Store(a.Input, x)
+				i.Store(a.Output, x)
+
+				if err := i.Execute(p); err != nil {
+					t.Fatal(err)
+				}
+
+				// Verify output.
+				output, ok := i.Load(a.Output)
+				if !ok {
+					t.Fatalf("missing output variable %q", a.Output)
+				}
+
+				expect := r.Chain.End()
+
+				if !bigint.Equal(output, expect) {
+					t.FailNow()
+				}
+			}
+		})
+	}
+}
